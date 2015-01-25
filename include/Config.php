269b021497d65cdd731509f1f283d8828f9006f0
Reza Khan <titan.550@gmail.com>
2015-01-24 16:05:12 -0800

    First Commit

---
+++ include/Config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'reza');
+define('DB_PASSWORD', '123');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'gnurse');
+ 
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+?>
